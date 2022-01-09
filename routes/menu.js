cd24d4bbeb9458b60911d682cf287b1767178a29
mata0050 <mata0050@outlook.com>
2022-01-09 07:52:23 -0500

    update item in  menu

--- routes/menu.js
+++ routes/menu.js
@@ -21,7 +21,7 @@ module.exports = (db) => {
       });
   });
 
-  // create  a new menu item
+  // update  a new menu item
   router.post("/", (req, res) => {
     // get userID from cookie
     // const userId = req.cookies['user_id'];
@@ -29,9 +29,8 @@ module.exports = (db) => {
     const { userId, name, description, price, photo_url, preparation_time } =
       req.body;
 
-    // check if user is admin
+    // check if user exists
     const queryString = "SELECT * FROM users WHERE id = $1";
-
 
     db.query(queryString, [userId])
       .then((data) => {
@@ -46,8 +45,8 @@ module.exports = (db) => {
         }
 
         const query =
-          "INSERT INTO public.menu_items( name, description, price, photo_url, preparation_time) VALUES ( $1, $2, $3, $4, $5) RETURNING *";
-        const values = [name, description, price, photo_url, preparation_time]
+          "INSERT INTO menu_items( name, description, price, photo_url, preparation_time) VALUES ( $1, $2, $3, $4, $5) RETURNING *";
+        const values = [name, description, price, photo_url, preparation_time];
 
         db.query(query, values)
           .then((data) => {
@@ -55,6 +54,88 @@ module.exports = (db) => {
             res.json({ menuItems });
 
             // returns new item
+          })
+          .catch((err) => {
+            res.status(500).json({ error: err.message });
+          });
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
+
+  // create  a new menu item
+  router.put("/", (req, res) => {
+    // get userID from cookie
+    // const userId = req.cookies['user_id'];
+    // NOTE: waiting for auth
+    const {
+      userId,
+      menu_item_id,
+      name,
+      description,
+      price,
+      photo_url,
+      preparation_time,
+    } = req.body;
+
+    // check if user exists
+    const queryString = "SELECT * FROM users WHERE id = $1";
+
+    db.query(queryString, [userId])
+      .then((data) => {
+        const user = data.rows;
+
+        if (user.length === 0) {
+          return res.status(404).send("Please login");
+        }
+
+        if (!user[0].admin) {
+          return res.status(404).send("Please login as Admin");
+        }
+
+        let newItem = {
+          name,
+          description,
+          price,
+          photo_url,
+          preparation_time,
+        };
+
+        if (name) newItem.name;
+        if (description) newItem.description;
+        if (price) newItem.price;
+        if (photo_url) newItem.photo_url;
+        if (preparation_time) newItem.preparation_time;
+
+        //find if item exists
+        const findMenuItem = "SELECT * FROM menu_items WHERE id = $1";
+        db.query(findMenuItem, [menu_item_id])
+          .then((data) => {
+            if (data.rows.length === 0) {
+              res.status(500).json({ error: err.message });
+            }
+
+            const query =
+              "UPDATE menu_items SET name = $1, description = $2, price = $3, photo_url = $4, preparation_time =$5 WHERE id = $6 RETURNING*";
+            const values = [
+              newItem.name,
+              newItem.description,
+              newItem.price,
+              newItem.photo_url,
+              newItem.preparation_time,
+              menu_item_id,
+            ];
+
+            db.query(query, values)
+              .then((data) => {
+                const menuItem = data.rows;
+                // returns update item
+                res.json(menuItem);
+              })
+              .catch((err) => {
+                res.status(500).json({ error: err.message });
+              });
           })
           .catch((err) => {
             res.status(500).json({ error: err.message });
