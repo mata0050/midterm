ef158d7bc55556f0739d5dacac745600d7291be1
mata0050 <mata0050@outlook.com>
2022-01-09 08:19:36 -0500

    delete menu item

--- routes/menu.js
+++ routes/menu.js
@@ -7,10 +7,12 @@
 
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
 
 module.exports = (db) => {
   // get all the menu items
-  router.get("/", (req, res) => {
+  router.get("/",
+   (req, res) => {
     db.query(`SELECT * FROM menu_items;`)
       .then((data) => {
         const menuItems = data.rows;
@@ -19,6 +21,89 @@ module.exports = (db) => {
       .catch((err) => {
         res.status(500).json({ error: err.message });
       });
+  });
+
+
+  // create  a new menu item
+  router.put("/", (req, res) => {
+    // get userID from cookie
+    // const userId = req.cookies['user_id'];
+    // NOTE: waiting for auth
+    const {
+      userId,
+      menu_item_id,
+      name,
+      description,
+      price,
+      photo_url,
+      preparation_time,
+    } = req.body;
+
+    // check if user exists
+    const queryString = "SELECT * FROM users WHERE id = $1";
+
+    db.query(queryString, [userId])
+    .then((data) => {
+      const user = data.rows;
+
+      if (user.length === 0) {
+        return res.status(404).send("Please login");
+      }
+
+      if (!user[0].admin) {
+        return res.status(404).send("Please login as Admin");
+      }
+
+      let newItem = {
+        name,
+        description,
+        price,
+        photo_url,
+        preparation_time,
+      };
+
+      if (name) newItem.name;
+      if (description) newItem.description;
+      if (price) newItem.price;
+      if (photo_url) newItem.photo_url;
+      if (preparation_time) newItem.preparation_time;
+
+      //find if item exists
+      const findMenuItem = "SELECT * FROM menu_items WHERE id = $1";
+      db.query(findMenuItem, [menu_item_id])
+      .then((data) => {
+        if (data.rows.length === 0) {
+          res.status(500).json({ error: err.message });
+        }
+
+        const query =
+        "UPDATE menu_items SET name = $1, description = $2, price = $3, photo_url = $4, preparation_time =$5 WHERE id = $6 RETURNING*";
+        const values = [
+          newItem.name,
+          newItem.description,
+          newItem.price,
+          newItem.photo_url,
+          newItem.preparation_time,
+          menu_item_id,
+        ];
+
+        db.query(query, values)
+        .then((data) => {
+          const menuItem = data.rows;
+          // returns update item
+          res.json(menuItem);
+        })
+        .catch((err) => {
+          res.status(500).json({ error: err.message });
+        });
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
   });
 
   // update  a new menu item
@@ -63,21 +148,12 @@ module.exports = (db) => {
         res.status(500).json({ error: err.message });
       });
   });
-
-  // create  a new menu item
-  router.put("/", (req, res) => {
+  // delete item from table
+  router.delete("/", (req, res) => {
     // get userID from cookie
     // const userId = req.cookies['user_id'];
     // NOTE: waiting for auth
-    const {
-      userId,
-      menu_item_id,
-      name,
-      description,
-      price,
-      photo_url,
-      preparation_time,
-    } = req.body;
+    const { userId, menu_item_id } = req.body;
 
     // check if user exists
     const queryString = "SELECT * FROM users WHERE id = $1";
@@ -94,48 +170,14 @@ module.exports = (db) => {
           return res.status(404).send("Please login as Admin");
         }
 
-        let newItem = {
-          name,
-          description,
-          price,
-          photo_url,
-          preparation_time,
-        };
+        const query = "DELETE FROM menu_items WHERE id = $1";
+        const values = [menu_item_id];
 
-        if (name) newItem.name;
-        if (description) newItem.description;
-        if (price) newItem.price;
-        if (photo_url) newItem.photo_url;
-        if (preparation_time) newItem.preparation_time;
-
-        //find if item exists
-        const findMenuItem = "SELECT * FROM menu_items WHERE id = $1";
-        db.query(findMenuItem, [menu_item_id])
+        db.query(query, values)
           .then((data) => {
-            if (data.rows.length === 0) {
-              res.status(500).json({ error: err.message });
-            }
+            const menuItems = data.rows;
+            res.json('Item deleted');
 
-            const query =
-              "UPDATE menu_items SET name = $1, description = $2, price = $3, photo_url = $4, preparation_time =$5 WHERE id = $6 RETURNING*";
-            const values = [
-              newItem.name,
-              newItem.description,
-              newItem.price,
-              newItem.photo_url,
-              newItem.preparation_time,
-              menu_item_id,
-            ];
-
-            db.query(query, values)
-              .then((data) => {
-                const menuItem = data.rows;
-                // returns update item
-                res.json(menuItem);
-              })
-              .catch((err) => {
-                res.status(500).json({ error: err.message });
-              });
           })
           .catch((err) => {
             res.status(500).json({ error: err.message });
