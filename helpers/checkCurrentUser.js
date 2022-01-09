d07b999622a2fb5cee861e2bd987fd32be8316f5
Ruta Reisoglu <rutareiso@protonmail.com>
2022-01-08 23:37:13 -0500

    Set up /:id/orders route. Check if correct user is logged in and show their orders

---
+++ helpers/checkCurrentUser.js
@@ -0,0 +1,16 @@
+//check if current logged in user is accessing their orders
+const checkCurrentUser = (session, userID, db) => {
+  return db
+    .query(`SELECT * FROM users WHERE id = $1;`, [userID])
+    .then((data) => {
+      const user = data.rows[0];
+      return user.id === Number(session);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
+};
+
+module.exports = {
+  checkCurrentUser,
+};
