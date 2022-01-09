141185f0b52971f4e5b8405ccc150c963ec1f7c8
Ruta Reisoglu <rutareiso@protonmail.com>
2022-01-09 16:15:38 -0500

    Pass data from database to views. Add selected items to localstorage on button click. Display flash message when item added to order.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,16 @@
-// Client facing scripts here
+console.log("rDDDDeact");
+$(document).ready(function () {
+  $(".order-btn").on("click", function (e) {
+    const menuItemID = $(this).parent().attr("id");
+    let amount = localStorage.getItem(menuItemID) || 0;
+    amount = Number(amount) + 1;
+    amount = JSON.stringify(amount);
+
+    localStorage.setItem(menuItemID, amount);
+    console.log(localStorage);
+    flash("Menu item added to order!", {
+      bgColor: "#fe744d",
+      ftColor: "black",
+    });
+  });
+});
