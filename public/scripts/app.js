a10c141dd6023d13f204fbaef3e91e6f43e8eb58
Ruta Reisoglu <rutareiso@protonmail.com>
2022-01-10 00:05:11 -0500

    Update dynamically user's order details using local storage. FLash a message when order has been received. Toggle order details

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,59 @@
+const addItemsToSumary = () => {
+  $(".summary-details").html("");
+
+  let totalQuantity = 0;
+  let totalPrice = 0;
+  let totalTime = 0;
+
+  const items = JSON.parse(localStorage.getItem("order"));
+  for (let itemID in items) {
+    const menuItem = $("body").find(`#${itemID}`);
+    const name = menuItem.children("[data-name]").attr("data-name");
+    const quantity = items[itemID];
+    const price = menuItem.children("[data-price]").attr("data-price");
+    const prepTime = menuItem.children("[data-time]").attr("data-time");
+    console.log({ name, price, prepTime });
+
+    totalQuantity += Number(quantity);
+    totalPrice += price * quantity;
+    totalTime += prepTime * quantity;
+
+    const itemDetails = `
+      <tr>
+      <td>${name}</td>
+      <td>${quantity}</td>
+      <td>${(price / 100).toFixed(2)}</td>
+      <td>${prepTime}</td>
+      </tr>
+    `;
+
+    $(".summary-details").append(itemDetails);
+  }
+
+  const tableHead = `
+  <tr>
+  <th>Menu Item</th>
+  <th>Quantity</th>
+  <th>Price</th>
+  <th>Prep time</th>
+  </tr>
+  `;
+  $(".summary-details").prepend(tableHead);
+
+  const total = `
+    <tr id="total">
+    <td>Total:</td>
+    <td>${totalQuantity}</td>
+    <td>${(totalPrice / 100).toFixed(2)}</td>
+    <td>${totalTime}</td>
+    </tr>
+  `;
+  $(".summary-details").append(total);
+};
+
 $(document).ready(function () {
+  addItemsToSumary();
+
   // save user's selected menu items to local storage
   $(".order-btn").on("click", function (e) {
     const order = JSON.parse(localStorage.getItem("order")) || {};
@@ -10,20 +65,45 @@ $(document).ready(function () {
     order[menuItemID] = amount;
 
     localStorage.setItem("order", JSON.stringify(order));
-    console.log(localStorage);
+    addItemsToSumary();
     flash("Menu item added to order!", {
       bgColor: "#fe744d",
       ftColor: "black",
     });
   });
 
-  // send user's selected items to POST /orders
+  // send user's selected menu items to POST /orders
   $("#place-order-btn").on("click", function (e) {
-    const data = JSON.parse(localStorage.getItem("order"));
-    $.ajax("/orders", { method: "POST", data })
-      .done((res) => console.log(res))
-      .fail(function (jqXHR, textStatus) {
-        console.error("Request failed: " + textStatus);
-      });
+    if (localStorage.getItem("order")) {
+      const data = JSON.parse(localStorage.getItem("order"));
+      $.ajax("/orders", { method: "POST", data })
+        .done((res) => {
+          console.log(res);
+          localStorage.clear();
+          $("#order-summary").hide();
+          addItemsToSumary();
+          flash(
+            "Order received! You will receive an SMS confirmation shortly!",
+            {
+              bgColor: "#962100",
+              ftColor: "white",
+              duration: 6000,
+            }
+          );
+        })
+        .fail(function (jqXHR, textStatus) {
+          console.error("Request failed: " + textStatus);
+        });
+    }
+  });
+
+  // display order-summary
+  $("#toggle-order-btn").on("click", function () {
+    $("#order-summary").animate(
+      {
+        height: "toggle",
+      },
+      600
+    );
   });
 });
--- routes/orders.js
+++ routes/orders.js
@@ -4,26 +4,26 @@ const router = express.Router();
 
 module.exports = (db) => {
   // -- create new order: -- //
-  router.post("/orders", (req, res) => {
-    const session = req.session.user_id;
+  router.post("/", (req, res) => {
+    const userID = req.session.user_id;
 
     console.log(req.body);
     // TO DO:
     // if session is admin proceed, otherwise forbidden
     // get all data about the order and save it in db, send SMS to user, send SMS to admin, res.redirect("/:id/orders);
+    res.send("New order received!");
+    // const query = `
 
-    const query = `
+    //     `;
+    // const values = [userID];
 
-        `;
-    const values = [userID];
+    // db.query(query, values)
+    //   .then(() => {})
+    //   .catch((err) => {
+    //     res.status(500).json({ error: err.message });
+    //   });
 
-    db.query(query, values)
-      .then(() => {})
-      .catch((err) => {
-        res.status(500).json({ error: err.message });
-      });
-
-    res.status(403).json({ error: "Forbidden!" });
+    // res.status(403).json({ error: "Forbidden!" });
   });
 
   return router;
