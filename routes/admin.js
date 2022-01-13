0cf26d5a14ebca73926397255ab1410f5aee9336
Ruta Reisoglu <rutareiso@protonmail.com>
2022-01-13 01:53:06 -0500

    Display order details

--- routes/admin.js
+++ routes/admin.js
@@ -49,6 +49,75 @@ module.exports = (db) => {
       });
   });
 
+  // @route    GET /admin/orders/:id
+  // @desc     Get order details
+  // @access   Private
+  router.get("/orders/:id", (req, res) => {
+    const session = req.session.user_id;
+    const orderID = req.params.id;
+    let templateVars = {};
+
+    // checks user
+    userUndefined(session, res);
+
+    // check if user exists and is Admin
+    const queryString = "SELECT * FROM users WHERE id = $1 AND admin = 'true'";
+
+    db.query(queryString, [session])
+      .then((data) => {
+        const user = data.rows;
+        if (user.length !== 0) {
+          // Order details
+          return db.query(
+            ` SELECT orders.id AS order_nr,
+            orders.status,
+            orders.created_at AS time_ordered, users.name,
+            SUM(menu_items.preparation_time) AS waiting_time,
+            SUM(menu_items.price) AS total_price
+            FROM orders
+            JOIN users ON users.id = user_id
+            JOIN selected_dishes ON orders.id = order_id
+            JOIN menu_items ON menu_items.id = menu_item_id
+            WHERE orders.id = $1
+            GROUP BY orders.id, users.name
+            ORDER BY orders.created_at DESC;
+            `,
+            [orderID]
+          );
+        }
+        res.redirect("/menu");
+        return;
+      })
+      .then((data) => {
+        const order = data.rows[0];
+        templateVars = {
+          userID: session,
+          order,
+        };
+        return db.query(
+          `SELECT COUNT(orders.id), menu_items.name FROM menu_items
+          JOIN selected_dishes ON menu_items.id = menu_item_id
+          JOIN orders ON orders.id = order_id
+          WHERE orders.id = $1
+          GROUP BY menu_items.id
+          `,
+          [orderID]
+        );
+      })
+      .then((data) => {
+        const items = data.rows;
+        templateVars.items = items;
+        console.log(templateVars);
+        res.render("admin_order_details", templateVars);
+        return;
+      })
+
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+        return;
+      });
+  });
+
   // @route    PUT /admin/orders/:order_id
   // @desc     Mark order as complete status
   // @access   Private
